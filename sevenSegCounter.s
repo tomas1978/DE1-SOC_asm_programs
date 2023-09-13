ddb99045532a2856d2b67ca655d7429434236d80
tomas1978 <tomas.nilsson78@gmail.com>
2023-09-13 12:02:10 +0200

    Start of project
    7-seg display shows 1, then 2

---
+++ sevenSegCounter.s
@@ -0,0 +1,22 @@
+.global _start
+_start:
+	//Create address 0xff20020 in register r5
+	mov r5, #0xff
+	lsl r5, r5, #24
+	mov r6, #0x20
+	lsl r6, r6, #16
+	add r5, r5, r6
+	mov r6, #0x20
+	add r5, r5, r6	
+	//Address to 7-seg display now in r5
+	
+	mov r1, #0x6
+	str r1, [r5]
+	
+	mov r1, #0x5b
+	str r1, [r5]
+	
+	
+
+	
+	
